6a864385f1a187e416b6ba59dcbad3504ad48deb
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-24 19:17:04 -0400

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Home());
@@ -6,7 +7,43 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: PokeGame(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PokeGame()),
+        ),
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          image: new AssetImage(
+            "images/splashscreen.jpg",
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
