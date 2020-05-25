2f4af1d8067303cfe536c8803768cdac318a3b79
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-05-25 16:43:34 -0400

    Added Story class

---
+++ lib/story.dart
@@ -0,0 +1,8 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  String choice3;
+
+  Story({String this.storyTitle, String this.choice1, String this.choice2, String this.choice3});
+}
